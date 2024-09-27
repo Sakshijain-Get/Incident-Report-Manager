207aa962fe7e77d2168e9ad44835c7e2eccab240
Sakshijain-Get <76477918+Sakshijain-Get@users.noreply.github.com>
2024-09-27 15:31:11 +0530

    Update SecurityConfig.java

--- src/main/java/com/security/IncidentReports/config/SecurityConfig.java
+++ src/main/java/com/security/IncidentReports/config/SecurityConfig.java
@@ -13,9 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
-            "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.html"};
-
+   private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
+			"/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+			"/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/api/auth/**",
+			"/api/test/**", "/authenticate" };
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
